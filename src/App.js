df13dbe5f83a0b4ef4741fa32015f4f295256f0c
Ithzamary <ithgarvi@gmail.com>
2021-01-18 21:59:18 -0600

    Se agrega nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import logo from './assets/beek-logo.svg'
 
 function App() {
   return (
-    <section className="container">
-        
-    </section>
+    <div className="main-container">
+      <header className="header">
+        <nav className="nav-header">
+          <ul className="items__nav-header">
+            <li>
+              <figure className="figure__logo-header">
+                <img src={logo} alt="imagenLogo" id="logo__img-header"/>
+              </figure>
+            </li>
+            <li className="item__centered">
+              <h2>Beek Challenge</h2>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
   );
 }
 
